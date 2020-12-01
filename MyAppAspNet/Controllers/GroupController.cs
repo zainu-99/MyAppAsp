1e52d2f4485f610c5648ad5b82aa582e6df2e4af
zainu-99 <zainu.developer@gmail.com>
2020-12-01 20:02:22 +0700

    Modified

---
+++ MyAppAspNet/Controllers/GroupController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAppAspNet.Controllers
+{
+    public class GroupController : Controller
+    {
+        // GET: Group
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Group/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Group/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Group/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Group/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Group/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Group/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Group/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MyAppAspNet/Controllers/GroupLevelController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAppAspNet.Controllers
+{
+    public class GroupLevelController : Controller
+    {
+        // GET: GroupLevel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: GroupLevel/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GroupLevel/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GroupLevel/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GroupLevel/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: GroupLevel/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GroupLevel/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GroupLevel/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
